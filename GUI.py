566f361eeacc8d0216c6c27855c35fc581620d87
Huskyinca <huskyinca@icloud.com>
2023-12-17 10:41:43 -0500

    Added the 'Edit' button

--- GUI.py
+++ GUI.py
@@ -4,20 +4,40 @@ import PySimpleGUI as sg
 label = sg.Text("Type in a to-do:")
 input_box=sg.InputText(tooltip="Enter todo", key="todo")
 add_button=sg.Button("Add")
+list_box=sg.Listbox(values=functions.get_todos(), key='todos',
+                    enable_events=True, size=[45,10])
+edit_button=sg.Button("Edit")
 
 window=sg.Window('My To-Do App',
-                 layout=[[label, input_box, add_button]],
+                 layout=[[label, input_box, add_button],
+                         [list_box,edit_button]],
                  font=('Helvetica',12))
 while True:
     event, value = window.read()
-    print(event)
-    print(value)
+    print(1,event)
+    print(2,value)
+    print(3,value['todos'])
     match event:
         case "Add":
             todos=functions.get_todos()
-            new_todo=value['todo'] + "\n"
+            new_todo=value['todo'].capitalize() + "\n"
             todos.append(new_todo)
             functions.write_todos(todos)
+            window['todos'].update(values=todos)
+            print(todos)
+        case "Edit":
+            todo_to_edit = value['todos'][0]
+            new_todo = value['todo']
+            new_todo = new_todo.capitalize() + "\n"
+
+            todos = functions.get_todos()
+            index=todos.index(todo_to_edit)
+            todos[index] = new_todo
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+            print(todos)
+        case 'todos':
+            window['todo'].update(value=value['todos'][0])
         case sg.WIN_CLOSED:
             break
 window.close()
